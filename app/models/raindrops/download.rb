ff394fdbf7e1bba003709f333b4a1820ebc7adb3
PNSalocin <nicolas.merelli@gmail.com>
2015-10-25 21:04:15 +0100

    feat(downloads): gestion des statuts
    Ajout de différents statuts pour l'avancement du téléchargement
    Ajout de gestion d'erreur sur l'ouverture ratée d'un fichier
    Refactorisation

--- app/models/raindrops/download.rb
+++ app/models/raindrops/download.rb
@@ -2,27 +2,28 @@ module Raindrops
   class Download < ActiveRecord::Base
 
     validates :source_url, presence: true
-    validates :source_url, format: { with: URI.regexp }, if: Proc.new { |a| a.source_url.present? }
+    validates :source_url, format: { with: URI.regexp }, if: proc { |a| a.source_url.present? }
     validates :destination_path, presence: true
 
+    after_initialize :default_values
+
+    enum status: { unprocessed: 0, downloading: 10, completed: 20,
+                   error_opening_file: -10 }
+
+    attr_accessor :verbose
+
+    # Démarre le téléchargement
     def start
       require 'net/http'
       Net::HTTP.new(source_uri.host, source_uri.port).request_get(source_uri.path) do |response|
-
-        file_size = response.header['Content-Length'].to_i
-
-        puts "File found @#{source_uri}."
-        puts "File size: #{file_size}."
-
-        puts "Opening destination file @#{destination_path}."
-        file = ::File.open(destination_path, "wb")
-        puts 'Destination file opened.'
+        puts "File found @#{source_uri}." if verbose
+        file_size = get_and_update_file_total_size response
+        return unless file = open_destination_file
+        self.update_attributes! status: Raindrops::Download.statuses[:downloading]
 
         bytes_downloaded = 0
         old_percent_downloaded = 0
         puts 'Starting download'
-
-        self.update_attributes! file_total_size: file_size
 
         response.read_body do |chunk|
           bytes_downloaded += chunk.length
@@ -38,23 +39,57 @@ module Raindrops
         end
 
         puts 'STOP DOWNLOAD'
-        self.update_attributes! completed: true
-
+        self.update_attributes! status: Raindrops::Download.statuses[:completed]
       end
     end
 
+    # Retourne le pourcentage actuel de progression de téléchargement du fichier
+    #
+    # *Returns* :
+    #   - _Integer_
     def progress
-      file_total_size && file_downloaded_size ?
-          file_downloaded_size / file_total_size * 100 :
-          0
+      file_total_size && file_downloaded_size ? (file_downloaded_size.to_f / file_total_size.to_f * 100).to_i : 0
     end
 
     private
 
+    # Assigne les valeurs par défaut au modèle
+    def default_values
+      self.verbose ||= true
+    end
+
+    def get_and_update_file_total_size(response)
+      file_total_size = response.header['Content-Length'].to_i
+      puts "File total size: #{file_total_size}." if verbose
+      self.update_attributes! file_total_size: file_total_size
+      file_total_size
+    end
+
+    # Retourne un objet URI correspondant à l'url source
+    #
+    # *Returns* :
+    #   - _URI_
     def source_uri
       @source_uri = URI(source_url) unless @source_uri
       @source_uri
     end
 
+    # Tente d'ouvrir le fichier de destination
+    def open_destination_file
+      puts "Trying to open destination file @#{destination_path}." if verbose
+      begin
+        file = ::File.open destination_path, 'wb'
+        puts 'Destination file opened.' if verbose
+        file
+      rescue SystemCallError => e
+        self.update_attributes! status: Raindrops::Download.statuses[:error_opening_file], error_content: e.message
+        false
+      end
+    end
+
+
+    def close_file
+
+    end
   end
 end
--- db/migrate/20151018085430_create_raindrops_downloads.rb
+++ db/migrate/20151018085430_create_raindrops_downloads.rb
@@ -3,9 +3,10 @@ class CreateRaindropsDownloads < ActiveRecord::Migration
     create_table :raindrops_downloads do |t|
       t.string :source_url, limit: 255, null: false
       t.string :destination_path, limit: 255, null: false
-      t.integer :file_total_size
-      t.integer :file_downloaded_size
-      t.boolean :completed, default: false
+      t.integer :file_total_size, null: false, default: 1
+      t.integer :file_downloaded_size, null: false, default: 0
+      t.integer :status, null: false, default: 0
+      t.string :error_content, limit: 250
       t.timestamps null: false
     end
   end
