eb9569b6e33c52c9ce53219cb178cb451b7c1242
PNSalocin <nicolas.merelli@gmail.com>
2015-10-27 08:45:12 +0100

    feat(sse): test!
    Envoi de dummy test dans un controlleur à part.
    Le streaming est live.
    Le site répond si d'autres appels sont effectués !

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Raindrops::Engine.routes.draw do
   root to: 'downloads#index'
   resources :downloads, only: [:index, :create, :destroy]
+  get 'downloads/progress', to: 'live#downloads_progress'
 end
